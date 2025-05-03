c1dc905021c398e44d5638242e5b044b1865fb62
Tubhaahmad <toubha.ah@hotmail.com>
2025-05-03 14:32:59 +0200

    Carousel functionality

---
+++ src/constants.mjs
@@ -0,0 +1,4 @@
+export const APIURL = "https://v2.api.noroff.dev";
+export const REGISTER_URL = "https://v2.api.noroff.dev/auth/register";
+export const LOGIN_URL = "https://v2.api.noroff.dev/auth/login";
+export const POSTS_URL = "https://v2.api.noroff.dev/blog/posts"; // You'll add /<username> dynamically
---
+++ src/index.mjs
@@ -0,0 +1,40 @@
+// CAROUSEL
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+//initializeSlider();
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+  }
+
+  document.querySelector(".prev").addEventListener("click", prevSlide);
+  document.querySelector(".next").addEventListener("click", nextSlide);
+}
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
